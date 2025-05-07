34e0342138b372827f6ab1aab26501a0ba092fba
조하은 <jhe93@naver.com>
2025-05-07 18:21:50 +0900

    BFS-(1697번)

---
+++ BFS/haeun/1697.js
@@ -0,0 +1,40 @@
+const input = () => {
+    const fs = require('fs');
+    const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+    return fs.readFileSync(filePath, 'utf-8').toString().trim().split(' ').map(Number);
+};
+
+const BFS = (sx, target) => {
+    const queue = [];
+    const dist = Array(100000 + 1).fill(Infinity);
+
+    // 수빈이의 위치
+    queue.push(sx);
+    dist[sx] = 0;
+
+    while (queue.length) {
+        const cx = queue.shift(); //현재 위치
+
+        for (let dx of [-1, 1, cx]) {
+            const nx = cx + dx; //다음 위치
+
+            //범위 벗어나는 경우 건너 뛰기
+            if (nx < 0 || nx > 100000) continue;
+
+            if (dist[nx] > dist[cx] + 1) {
+                dist[nx] = dist[cx] + 1;
+                queue.push(nx);
+            }
+        }
+    }
+
+    return dist[target];
+};
+
+const solution = () => {
+    const [N, K] = input();
+    console.log(BFS(N, K));
+};
+
+solution();
